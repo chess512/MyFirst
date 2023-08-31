26e7803e389ae1d0fecf6311d177d27af5e9d61e
Srinivas Halembar <srinivas.halembar@gmail.com>
2023-08-30 23:48:46 -0400

    Initial Commit

---
+++ MyFirst/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  MyFirst
+//
+//  Created by Srini on 8/30/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Srini on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
